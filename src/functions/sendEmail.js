bec35ab00bd40515eac9b743fb14fc98a19de5db
BakangMonei <bakangmonei2@gmail.com>
2024-04-26 14:29:26 +0200

    vugh

--- src/functions/sendEmail.js
+++ src/functions/sendEmail.js
@@ -1,8 +1,8 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 
-const gmailEmail = 'bakangmonei2@gmail.com';
-const gmailPassword = 'WatchmanInc@959115515';
+const gmailEmail = '';
+const gmailPassword = '';
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
